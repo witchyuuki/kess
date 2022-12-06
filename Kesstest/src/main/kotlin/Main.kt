655934061451c698952059b3bbc414e9ff5ebaba
DMC-08 <646051200079@mail.rmutk.ac.th>
2022-12-06 16:45:55 +0700

    eiei

---
+++ Kesstest/src/main/kotlin/Main.kt
@@ -0,0 +1,52 @@
+
+fun main(args: Array<String>) {
+    print("Enter N:")
+    var y = readLine()!!.toInt()
+    for (i in y downTo 1) {
+        for (j in 1..y) {
+            print("* ")
+        }
+        println()
+    }
+
+        val rows = 5
+        var k = 0
+
+        for (i in 1..rows) {
+            for (space in 1..rows - i) {
+                print("  ")
+            }
+
+            while (k != 2 * i - 1) {
+                print("* ")
+                ++k
+            }
+
+            println()
+            k = 0
+        }
+
+    for (i in rows downTo 1) {
+
+        for (space in 1..rows - i) {
+            print("  ")
+        }
+
+        for (j in i..2 * i - 1) {
+            print("* ")
+        }
+
+        for (j in 0..i - 1 - 1) {
+            print("* ")
+        }
+
+        println()
+    }
+}
+
+
+
+
+
+
+
